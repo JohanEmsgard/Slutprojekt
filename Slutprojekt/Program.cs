c583a02a1c466dd49162275f73e5e18855509018
Johan Emsgård <emsgard.johan@elev.ga.ntig.se>
2022-03-22 14:24:18 +0100

    hbdh

--- Slutprojekt/Program.cs
+++ Slutprojekt/Program.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using Raylib_cs;
 
+//För kärmens storlek
 Raylib.InitWindow(1000, 800, "Platformer");
 Raylib.SetTargetFPS(60);
 
-
+//För skärmens background
 while(!Raylib.WindowShouldClose())
 {
     Raylib.BeginDrawing();
-    Raylib.ClearBackground(Color.GREEN);
+    Raylib.ClearBackground(Color.GREEN); 
+    Raylib.DrawRectangle(100,100,100,100,Color.BLUE);
+
+    //Så programmet inte stängs av på dirketen
     Raylib.EndDrawing();
 }
+
 
 Console.ReadLine();
