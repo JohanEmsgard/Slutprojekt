055c96259216c9f2df17ac4453c51e09d3334f8c
Johan Emsgård <emsgard.johan@elev.ga.ntig.se>
2022-03-29 14:16:52 +0200

    hgjdfhjdf

--- Slutprojekt/Program.cs
+++ Slutprojekt/Program.cs
@@ -1,21 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using Raylib_cs;
+using System.Numerics;
 
 //För kärmens storlek
 Raylib.InitWindow(1000, 800, "Platformer");
 Raylib.SetTargetFPS(60);
+
+// X och Y. Den har två axlar. 
+Vector2 position = new Vector2(10, 10);
 
 //För skärmens background
 while(!Raylib.WindowShouldClose())
 {
     Raylib.BeginDrawing();
     Raylib.ClearBackground(Color.GREEN); 
-    Raylib.DrawRectangle(100,100,100,100,Color.BLUE);
+    Raylib.DrawRectangle((int)position.X,(int)position.Y,100,100,Color.BLUE);
 
     //Så programmet inte stängs av på dirketen
     Raylib.EndDrawing();
+
+    //Så vilka knappar är movement och hur fort dom gör på sig.
+    if(Raylib.IsKeyDown(KeyboardKey.KEY_A)) position.X -= 10;
+    if(Raylib.IsKeyDown(KeyboardKey.KEY_S)) position.X += 10;
+    if(Raylib.IsKeyDown(KeyboardKey.KEY_W)) position.Y -= 10;
+    if(Raylib.IsKeyDown(KeyboardKey.KEY_Z)) position.Y += 10;
 }
+
+
+
 
 
 Console.ReadLine();
